3ed326adaa0f689ad088477627615374e25f049c
niklas eriksson <niklaseriksson32@hotmail.com>
2024-02-27 12:47:40 +0000

    function to create random ship

--- run.py
+++ run.py
@@ -8,3 +8,12 @@ for x in range(0,5):
 def print_board(board):
     for row in board:
         print((" ").join(row))
+
+
+# creates random ships on board
+def random_row(board):
+    return randint(0, len(board) - 1)
+  
+   
+def random_col(board):
+     return randint(0, len(board) - 1)
